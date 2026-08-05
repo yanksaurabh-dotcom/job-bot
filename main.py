ac60d6445b55024b143f37e792a8c3e9f6a9c279
yanksaurabh-dotcom <yanksaurabh@gmail.com>
2026-08-06 00:53:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 from scrapers.sarkariresult import get_latest
+from database import is_new
+from formatter import make_post
 from telegram_sender import send_message
+import time
 
-jobs = get_latest()
+try:
+    jobs = get_latest()
 
-msg = ""
+    if not jobs:
+        send_message("❌ Homepage se koi update nahi mila.")
+        raise SystemExit()
 
-for i, job in enumerate(jobs, 1):
-    msg += f"{i}. {job['title']}\n\n"
+    new_count = 0
 
-send_message(msg[:4000])
+    for job in jobs:
+
+        if is_new(job["link"]):
+
+            msg = make_post(
+                "SarkariResult",
+                job["title"],
+                job["link"]
+            )
+
+            send_message(msg)
+
+            new_count += 1
+
+            # Telegram rate limit se bachne ke liye
+            time.sleep(2)
+
+    if new_count == 0:
+        print("No new updates found.")
+
+except Exception as e:
+    send_message(f"❌ ERROR\n\n{e}")
