3e0436f02f3500b16c235519c82b01a63a7cc0fc
yanksaurabh-dotcom <yanksaurabh@gmail.com>
2026-08-06 00:20:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import requests
+import cloudscraper
 from telegram_sender import send_message
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/138.0 Safari/537.36"
-}
+scraper = cloudscraper.create_scraper()
 
 try:
-    r = requests.get(
-        "https://www.sarkariresult.com/",
-        headers=headers,
-        timeout=30
-    )
+    r = scraper.get("https://www.sarkariresult.com/", timeout=30)
 
     send_message(
-        f"Status Code : {r.status_code}\nLength : {len(r.text)}"
+        f"Status : {r.status_code}\nLength : {len(r.text)}"
     )
 
 except Exception as e:
