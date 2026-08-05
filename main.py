028142e0c46c166f1ab4268939ccaf48ececc84f
yanksaurabh-dotcom <yanksaurabh@gmail.com>
2026-08-06 00:25:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram_sender import send_message
 scraper = cloudscraper.create_scraper()
 
 try:
-    r = scraper.get("https://www.sarkariresult.com/", timeout=30)
+    r = scraper.get("https://www.sarkariresult.com.cm/", timeout=30)
 
     send_message(
         f"Status : {r.status_code}\nLength : {len(r.text)}"
