c6b6b01317aa26e86bd056ee04f4fedb08834f9a
yanksaurabh-dotcom <yanksaurabh@gmail.com>
2026-08-06 01:41:23 +0530

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -1,26 +1,26 @@
 import requests
 from config import BOT_TOKEN, CHAT_ID
 
-API = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 
 def send_message(text):
-    try:
-        r = requests.post(
-            API,
-            data={
-                "chat_id": CHAT_ID,
-                "text": text,
-                "parse_mode": "HTML",
-                "disable_web_page_preview": False,
-            },
-            timeout=30,
-        )
 
-        if r.status_code == 200:
-            print("Telegram Message Sent")
-        else:
-            print(r.text)
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": text,
+        "parse_mode": "HTML",          # ✅ HTML formatting
+        "disable_web_page_preview": False
+    }
 
-    except Exception as e:
-        print(e)
+    r = requests.post(
+        URL,
+        json=payload,
+        timeout=30
+    )
+
+    if not r.ok:
+        print("Telegram Error:")
+        print(r.text)
+
+    return r
