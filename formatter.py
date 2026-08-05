b54c1040f55ae282cfd89ffc4857a8d17692203f
yanksaurabh-dotcom <yanksaurabh@gmail.com>
2026-08-05 23:59:49 +0530

    Create formatter.py

---
+++ formatter.py
@@ -0,0 +1,12 @@
+def make_post(source, title, link):
+    return f"""📢 <b>{title}</b>
+
+🏛 <b>Source:</b> {source}
+
+🔗 <a href="{link}">Read Full Details</a>
+
+━━━━━━━━━━━━━━━━━━
+
+🤖 Auto Update
+📲 @jobupdatesbihar
+"""
