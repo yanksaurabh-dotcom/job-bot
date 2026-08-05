ee64f664d7d5866b2977611857ce83e221ce70fc
yanksaurabh-dotcom <yanksaurabh@gmail.com>
2026-08-06 01:09:49 +0530

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,12 +1,36 @@
-def make_post(source, title, link):
-    return f"""📢 <b>{title}</b>
+def make_post(data, link):
 
-🏛 <b>Source:</b> {source}
+    msg = f"📢 {data['title']}\n\n"
 
-🔗 <a href="{link}">Read Full Details</a>
+    if data["description"]:
+        msg += f"📝 {data['description']}\n\n"
 
-━━━━━━━━━━━━━━━━━━
+    if data["post"]:
+        msg += "🔥 Total Posts\n"
+        msg += data["post"] + "\n\n"
 
-🤖 Auto Update
-📲 @jobupdatesbihar
-"""
+    if data["dates"]:
+        msg += "📅 Important Dates\n"
+        msg += data["dates"] + "\n\n"
+
+    if data["fee"]:
+        msg += "💰 Application Fee\n"
+        msg += data["fee"] + "\n\n"
+
+    if data["age"]:
+        msg += "👤 Age Limit\n"
+        msg += data["age"] + "\n\n"
+
+    if data["eligibility"]:
+        msg += "🎓 Eligibility\n"
+        msg += data["eligibility"][:700] + "\n\n"
+
+    if data["selection"]:
+        msg += "📝 Selection Process\n"
+        msg += data["selection"] + "\n\n"
+
+    msg += "━━━━━━━━━━━━━━━━━━\n"
+    msg += f"🔗 Read Full Details\n{link}\n\n"
+    msg += "🤖 @jobupdatesbihar"
+
+    return msg
