a391a99fdc10df29575b1ed22674551fc89cd928
yanksaurabh-dotcom <yanksaurabh@gmail.com>
2026-08-06 00:15:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import requests
 from telegram_sender import send_message
 
-try:
-    r = requests.get("https://www.sarkariresult.com", timeout=30)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/138.0 Safari/537.36"
+}
 
-    if r.status_code == 200:
-        send_message("✅ JobBot Bihar Started Successfully!\n\nSarkari Result website is reachable.")
-    else:
-        send_message(f"❌ Website Error : {r.status_code}")
+try:
+    r = requests.get(
+        "https://www.sarkariresult.com/",
+        headers=headers,
+        timeout=30
+    )
+
+    send_message(
+        f"Status Code : {r.status_code}\nLength : {len(r.text)}"
+    )
 
 except Exception as e:
-    send_message(f"❌ Error\n\n{e}")
+    send_message(str(e))
